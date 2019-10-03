f4e8267e72e810eafefbfeb860d0c76453564cd6
Caio Martinelli <levanto.code@gmail.com>
2019-10-03 16:11:42 -0300

    Alert change working hours

---
+++ code/js/modal-horas-trabalhadas.js
@@ -0,0 +1,18 @@
+$('#informar-horas-trabalhadas')
+    .on('click', () => {
+        $('#aviso-horas-trabalhadas').modal('show')
+    })
+;
+
+$('#btn-aviso-cancelar')
+    .on('click', () => {
+        $('#aviso-horas-trabalhadas').modal('hide')
+    })
+;
+
+$('#btn-aviso-aceitar')
+    .on('click', () => {
+
+        $('#aviso-horas-trabalhadas').modal('hide')
+    })
+;
